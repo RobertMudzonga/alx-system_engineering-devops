3637ea78bdfe148a3d018a6373f419ff77f94d9e
RobertMudzonga <robertmudzonga@yahoo.com>
2022-04-26 16:17:12 +0000

    kinda fixed 0x16

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,52 @@
 #!/usr/bin/python3
-"""Module for task 3"""
+"""
+    Recursive function that queries the Reddit API, parses the title of all hot
+    articles, and prints a sorted count of given keywords (case-insensitive,
+    delimited by spaces. Javascript should count as javascript, but java should
+    not).
+"""
+import re
+import requests
+headers = {'user-agent': 'ubuntu:hbtn:v1.0\
+ (by /u/Brandixitor)'}
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Queries the Reddit API and returns the count of words in
-    word_list in the titles of all the hot posts
-    of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+def count_words(subreddit, word_list, after='', occurs={}):
+    """
+        Recursive function that queries the Reddit API, parses the title of all
+        hot articles, and prints a sorted count of given keywords
+        (case-insensitive, delimited by spaces.
+        Javascript should count as javascript, but java should not).
+    """
+    url = 'https://api.reddit.com/r/' + subreddit + '?limit=100&after=' + after
+    response = requests.get(url, headers=headers)
+    try:
+        data = response.json()
+    except:
+        return
+    if (str(response.status_code) == '404'):
+        return
+    dataLength = len(data['data']['children'])
+    if (dataLength is 0):
+        return
+    for i in range(0, dataLength):
+        try:
+            get_title = data['data']['children'][i]['data']['title']
+            for a in word_list:
+                try:
+                    occurs[a]
+                except KeyError:
+                    occurs[a] = 0
+                finally:
+                    occurs[a] += re.subn(r'(?i)(?<!\S)\b{}\b(?!\S)'.format(a),
+                                         '', get_title)[1]
+        except:
+            pass
+    afterVal = data['data']['after']
+    if (afterVal is not None):
+        return count_words(subreddit, word_list, afterVal, occurs)
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        for key in sorted(occurs, key=lambda k: (-occurs[k], k)):
+            if (occurs[key] > 0):
+                print("{}: {}".format(key, occurs[key]))
+        return
